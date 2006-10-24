0bdec38727465f2d78924bf2df9c7ca03089e8c3
smceligot <smceligot>
2006-10-24 02:06:00 +0000

    updated guile wrapper

--- swig/guile/test.sh
+++ swig/guile/test.sh
@@ -2,10 +2,11 @@
 rm -vf __db.* *.db
 LD_LIBRARY_PATH=.libs:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
-guile -s mktable.scm testtable 2 strcol 4 intcol 2> test.log
-guile -s desc.scm testtable 2>>test.log
-guile -s addrow.scm testtable one 1 2>>test.log
-guile -s addrow.scm testtable two 2 2>>test.log
-guile -s addrow.scm testtable three 3 2>>test.log
-guile -s list.scm testtable 2>>test.log
+GUILE="libtool --mode=execute guile"
+$GUILE -s mktable.scm testtable 2 strcol 4 intcol 2> test.log
+$GUILE -s desc.scm testtable 2>>test.log
+$GUILE -s addrow.scm testtable one 1 2>>test.log
+$GUILE -s addrow.scm testtable two 2 2>>test.log
+$GUILE -s addrow.scm testtable three 3 2>>test.log
+$GUILE -s list.scm testtable 2>>test.log
 
